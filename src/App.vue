e8c0eb1d0dc3d8976e534cf18f3b4d196cc7e240
blake <zuttton@gmail.com>
2018-08-15 13:33:10 -0400

    Huge UI change. New home and style.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" fixed right app>
+      <v-subheader style="margin-bottom:-30px">SEARCH FOR AWESOMES</v-subheader>
+      <v-text-field hide-details prepend-icon="search" single-line class="pa-3"></v-text-field>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-subheader>CHOOSE YOUR AWESOMES</v-subheader>
+        <v-list-group v-for="subject in Object.keys(AwesomeData)" :key="subject">
+          <v-list-tile slot="activator">
+            <v-icon class="hidden-xs-only pr-3">home</v-icon>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subject }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list v-for="(subitems) in AwesomeData[subject]" :key="subitems.name" dense>
+
+            <v-list-tile :href="subitems.url">
+              <v-icon>subdirectory_arrow_right</v-icon>
+              <v-list-tile-action>{{ subitems.name }}</v-list-tile-action>
+              <v-spacer></v-spacer>
+              <v-btn round small icon color="grey" @click="router.push('/e')">
+                <v-icon style="color:white">star</v-icon>
+              </v-btn>
+            </v-list-tile>
+          </v-list>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar app>
       <v-toolbar-title>Awesomely</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -13,5 +42,20 @@
 </template>
 
 <script>
+import AwesomeData from '../static/awesome.json'
 
+  export default {
+    data: () => ({
+      AwesomeData,
+      drawer: null,
+    }),
+    props: {
+      source: String
+    },
+     methods: {
+      select: function() {
+       window.open = this.subitems.url;
+    }
+  }
+  }
 </script>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container fluid class="px-3">
+    <v-layout justify-center align-center row>
+      <v-flex md9>
+        <v-card>
+          <v-card-media src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px">
+          </v-card-media>
+
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Making your resources more convenient</div>
+              <span class="grey--text">Explore thousands of resources in a convenient manner.</span>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat>Share</v-btn>
+            <v-btn flat color="purple">Explore</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="show = !show">
+              <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-slide-y-transition>
+            <v-card-text v-show="show">
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </v-slide-y-transition>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      show: false
+    })
+  }
+</script>
+
+<style>
+</style>
